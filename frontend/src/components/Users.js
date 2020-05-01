f309d5be08c1708b3899684015a5e1d769291f2b
“takuma” <fsww2353@gmail.com>
2020-05-01 13:45:35 +0900

    createUser コンポーネント

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import UserAvatar from './UserAvatar';
-// import CreateUser from './CreateUser';
+import CreateUser from './CreateUser';
 
 const GET_USERS = gql`
 {
@@ -21,6 +21,14 @@ function Users({selectUser}) {
   if (loading) return 'Loading...';
   if (error) return `Error ${error.message}`;
 
+  function updateUsers(cache, { data: { createUser }}) {
+    const { users } = cache.readQuery({ query: GET_USERS });
+    cache.writeQuery({
+      query: GET_USERS,
+      data: { users: users.concat([createUser.user]) },
+    });
+  }
+
   return(
     <div className="flex flex-wrap items-center pb-16">
       {data.users.map(user => (
@@ -28,6 +36,8 @@ function Users({selectUser}) {
           <UserAvatar user={user} />
         </div>
       ))}
+
+      <CreateUser onCreateUser={updateUsers} />
     </div>
   )
 }
